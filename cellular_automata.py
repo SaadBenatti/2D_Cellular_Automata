eb4044129f533a064d5153e085b9979725fa6891
Saad <72278165+SaadBenatti@users.noreply.github.com>
2021-11-25 22:10:16 -0500

    Add files via upload

--- cellular_automata.py
+++ cellular_automata.py
@@ -1,9 +1,6 @@
-from os import system
-import numpy as np
 import random, math, time
 # from numba import jit
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import pygame
 
 
@@ -16,25 +13,24 @@ class CA(object):
             self.nb_kelp = 0
             self.urchin_pos = []  # array of position of all urchins for otter hunt
             self.empty = []
-            # creates the 1d array
+            # creates the 2d array
             self.grid = []
             for i in range(100):
                 self.grid.append([])
 
-            for i in range(100):  # sets initial density : 0.1% otter, 64.9% empty, 30% kelp, 5% urchin
+            for i in range(100):  # sets initial density : 0.1% otter, 59.9% empty, 30% kelp, 10% urchin
                 for j in range(100):
                     temp = random.randint(1, 1000)
                     if temp == 1:
                         self.grid[i].append(Otter([i, j]))
                         self.nb_otter += 1
 
-                    elif 2 <= temp <= 301:
+                    elif 2 <= temp <= 101:
                         self.grid[i].append(Urchin([i, j]))
                         self.urchin_pos.append([i, j])
                         self.nb_urchin += 1
-                        
 
-                    elif 301 <= temp <= 306:
+                    elif 102 <= temp <= 401:
                         self.grid[i].append(Kelp([i, j]))
                         self.nb_kelp += 1
 
@@ -47,17 +43,12 @@ class CA(object):
     # kill otters if too old
     # kill urchins if too old
     # kill kelp if too old
-
     # make otters move
     # make urchins move
     # reproduce otters
     # reproduce urchins
     # reproduce kelp
     # make them age +1
-    # display grid
-    # plot numbers
-
-    # maybe not pass by reference?
     def updating(self):
 
         # KILLING THE OLD ANIMALS
@@ -73,12 +64,9 @@ class CA(object):
                     self.grid[i][j].move(self)
 
         # MOVING THE URCHINS
-        k = 0
         for i in range(100):
             for j in range(100):
                 if type(self.grid[i][j]) is Urchin:  # checks if animal is an Urchin
-                    print(k)
-                    k += 1
                     self.grid[i][j].move(self)
 
         # REPRODUCTION FOR OTTERS
@@ -92,29 +80,27 @@ class CA(object):
             for j in range(100):
                 if type(self.grid[i][j]) is Urchin:  # checks if animal is an urchin:
                     self.grid[i][j].reproduce(self)
-        print()
+
         # REPRODUCTION FOR KELP
         for i in range(100):
             for j in range(100):
                 if type(self.grid[i][j]) is Kelp:  # checks if animal is an kelp:
                     self.grid[i][j].reproduce(self)
-        print()
+
         # MAKING THEM AGE +1
         for i in range(100):
             for j in range(100):
                 if self.grid[i][j] != [0]:
                     self.grid[i][j].update_age()
 
-                    # fig,ax=plt.subplots(figsize=(16,9))
-        # ax.matshow(self.cellular_automaton)
-        # ax.axis(False)
-
+    # initializes display screen
     def create_display(self):
         white = (255, 255, 255)
         screen = pygame.display.set_mode((1000, 1000))
         screen.fill(white)  # set background white
         return screen
 
+    # updates display screen after every iteration
     def display(self, screen):
         URCHIN_COLOR = (255, 0, 0)  # Urchin is red
         KELP_COLOR = (0, 255, 0)  # Kelp is green
@@ -148,16 +134,6 @@ class CA(object):
                 rect = pygame.Rect(x, y, blockSize, blockSize)
                 pygame.draw.rect(screen, black, rect, 1)
 
-        # running = True
-        # while running:
-        # # for loop through the event queue
-        #     for event in pygame.event.get():
-
-        #         # Check for QUIT event
-        #         if event.type == pygame.QUIT:
-        #             running = False
-
-
 class Animal:
     def __init__(self, grid_position, reprod_chance, reprod=None, old=None, has_Moved=False):
         self.age = 0  # sets age to 0 when born
@@ -168,7 +144,9 @@ class Animal:
         self.position_Y = grid_position[1]
         self.reprod_chance = reprod_chance
 
-    def is_dead(self) -> bool:  # checks if it reached old age
+    def is_dead(self) -> bool:  # checks if it reached old age or hasn't eaten in a long time
+        if type(self) is Urchin and self.has_eaten>=5:
+            return True
         return self.age >= self.old
 
     def death(self, CA):
@@ -185,7 +163,6 @@ class Animal:
             CA.empty.append([self.position_X, self.position_Y])
         else:
             pass
-
         return boo
 
     def can_reproduce(self) -> bool:  # checks if creature reached reproduction age
@@ -198,16 +175,15 @@ class Animal:
         self.age += 1
         return self.age
 
-
 class Kelp(Animal):
     def __init__(self, grid_position):
-        super().__init__(grid_position, 90, 1, 3)  # sets reprod_prob=90,reprod_age=1, death_age=3
+        super().__init__(grid_position, 100, 1, 3)  # sets reprod_prob=100,reprod_age=1, death_age=3
 
     def reproduce(self, CA):
         if not self.can_reproduce():
             return CA
 
-        if self.reprod_chance >= random.randint(0, 100):  # how many digits ?
+        if self.reprod_chance >= random.randint(0, 100):
             if (len(CA.empty)) == 0:
                 return CA
             else:
@@ -218,10 +194,8 @@ class Kelp(Animal):
                 CA.grid[new_X][new_Y] = Kelp([new_X, new_Y])
                 # updating the number of kelps
                 CA.nb_kelp += 1
-
                 # deleting from the empty the location of the new kelp
                 CA.empty.pop(temp)
-
                 return CA
 
     def death(self, CA):
@@ -230,12 +204,10 @@ class Kelp(Animal):
         # delete(self)
         return CA
 
-
 class Urchin(Animal):
     def __init__(self, grid_position):
-        super().__init__(grid_position, 1, 2, 19)  # sets reprod_prob=1%,reprod_age=2, death_age=19
-
-        self.has_eaten = False
+        super().__init__(grid_position, 6, 2, 19)  # sets reprod_prob=6%,reprod_age=2, death_age=19
+        self.has_eaten =0 # tracks how long it has been since urchin ate
 
     # RETURNS THE GRID
     def move(self, CA):
@@ -244,11 +216,11 @@ class Urchin(Animal):
             return CA
 
         availablePositionsToMove = self.can_move(CA)
-        # print(availablePositionsToMove)
 
         if len(availablePositionsToMove) == 0:
             self.has_moved = False
             return CA
+
         else:
             temp = random.randint(0, len(availablePositionsToMove) - 1)
             new_position_X = availablePositionsToMove[temp][0]
@@ -258,13 +230,11 @@ class Urchin(Animal):
             # updating the empty array from the grid
             CA.empty.append([self.position_X, self.position_Y])
             CA.urchin_pos.remove([self.position_X, self.position_Y])
-            # Adding the same Urchin to the new location
-            # CA.nb_urchin += 1
             if type(CA.grid[new_position_X][new_position_Y]) is Kelp:
                 CA.nb_kelp -= 1
-                self.has_eaten = True  # urchin ate kelp
+                self.has_eaten =0 # urchin ate kelp
             else:
-                self.has_eaten = False  # urchin didnt eat kelp
+                self.has_eaten +=1  # urchin didn't eat kelp
                 CA.empty.remove([new_position_X, new_position_Y])
 
             CA.grid[new_position_X][new_position_Y] = self
@@ -272,15 +242,12 @@ class Urchin(Animal):
             self.position_Y = new_position_Y
             # adding to urchin array
             CA.urchin_pos.append([new_position_X, new_position_Y])
-            # print(CA.empty)
-            # print(availablePositionsToMove[temp])
             self.has_moved = True
             return CA
 
     # RETURNS THE ARRAY OF AVAILABLE POSITIONS
     def can_move(self, CA):
         # checking bounds
-
         moveDistance = 2
         limit_x1 = 2
         limit_x2 = 2
@@ -314,50 +281,46 @@ class Urchin(Animal):
                 limit_y2 = 1
 
         # now checking if he can actually move - Aka is everything filled ?
-        # To do: debug
         for i in range(self.position_X - limit_x1, self.position_X + limit_x2 + 1):
             for j in range(self.position_Y - limit_y1, self.position_Y + limit_y2 + 1):
                 if CA.grid[i][j] == [0] or type(CA.grid[i][j]) is Kelp:
                     availablePositionsToMove.append([i, j])
-
-        # print(availablePositionsToMove)
 
         return availablePositionsToMove
 
     def reproduce(self, CA):
         if not self.can_reproduce():
             return CA
-        if self.reprod_chance >= random.randint(0, 100):  # how many digits ?
-            if (len(CA.empty)) == 0:
+        if self.reprod_chance >= random.randint(0, 100):
+            positions = self.can_move(CA)  # adds urchin to empty or kelp square
+            if (len(positions)) == 0:
                 return CA
+
             else:
-                temp = random.randint(0, len(CA.empty) - 1)
-                new_X = CA.empty[temp][0]
-                new_Y = CA.empty[temp][1]
-                # add a new urchin on the random empty location
+                temp = random.randint(0, len(positions) - 1)
+                new_X = positions[temp][0]
+                new_Y = positions[temp][1]
+                if type(CA.grid[new_X][new_Y]) is Kelp:
+                    CA.nb_kelp-=1
+                else:
+                    CA.empty.remove([new_X, new_Y])  # deleting from the empty the location of the new urchin
+                # add a new urchin on the random location
                 CA.grid[new_X][new_Y] = Urchin([new_X, new_Y])
                 # adding to urchin array
                 CA.urchin_pos.append([new_X, new_Y])
-
                 CA.nb_urchin += 1
-
-                # deleting from the empty the location of the new urchin
-                CA.empty.pop(temp)
-
                 return CA
 
     def death(self, CA):
         if super().death(CA):
             CA.nb_urchin -= 1
-        # CA.urchin_pos.remove([self.position_X, self.position_Y])
-        # delete(self)
-        return CA
 
+        return CA
 
 class Otter(Animal):
     def __init__(self, grid_position):
         super().__init__(grid_position, 20, 4, 20)  # sets reprod_prob=20,reprod_age=4, death_age=20
-
+        self.carrying_capacity=10 # sets carrying capacity to 10
     def has_space(self, CA):  # used for reproduction
         # checking bounds
 
@@ -384,7 +347,7 @@ class Otter(Animal):
         # now checking if he can actually move - Aka is everything filled ?
         for i in range(self.position_X - limit_x1, self.position_X + limit_x2 + 1):
             for j in range(self.position_Y - limit_y1, self.position_Y + limit_y2 + 1):
-                if CA.grid[i][j] == [0]:
+                if CA.grid[i][j] == [0] or type(CA.grid[i][j]) is Kelp or type(CA.grid[i][j]) is Urchin:
                     availablePositionsToMove.append([i, j])
         return availablePositionsToMove
 
@@ -398,8 +361,6 @@ class Otter(Animal):
             CA.grid[self.position_X][self.position_Y] = [0]  # sets grid to empty
             CA.empty.append([self.position_X, self.position_Y])  # updating empty array
             move_location = CA.urchin_pos[temp]  # chooses random location tuple in urchin pos
-            # print(move_location)
-            # print(move_location[0])
             CA.grid[move_location[0]][move_location[1]] = self  # sets new location to otter
             CA.urchin_pos.pop(temp)  # deletes value from urchin pos
             CA.nb_urchin -= 1
@@ -410,14 +371,13 @@ class Otter(Animal):
         self.has_moved = True
         return CA
 
-    #To Do: Should Otter be able to reproduce on kelp squares?
     def reproduce(self, CA):
         if not self.can_reproduce():
             return CA
-        if CA.nb_otter < 2 or CA.nb_otter >= 10:
+        if CA.nb_otter < 2 or CA.nb_otter >= self.carrying_capacity:
             return CA
 
-        if self.reprod_chance >= random.randint(0, 100):  # how many digits ?
+        if self.reprod_chance >= random.randint(1, 100):
             if (len(CA.empty)) == 0:
                 return CA
 
@@ -428,31 +388,49 @@ class Otter(Animal):
             else:
                 temp = random.randint(0, len(positions) - 1)
                 new_location = positions[temp]
-                CA.grid[new_location[0]][new_location[1]] = Otter(
-                    [new_location[0], new_location[1]])  # spawns new otter
-                CA.empty.remove([new_location[0], new_location[1]])  # delete from empty
+                if type(CA.grid[new_location[0]][new_location[1]]) is Kelp:
+                    CA.nb_kelp -= 1
+                elif type(CA.grid[new_location[0]][new_location[1]]) is Urchin:
+                    CA.urchin_pos.remove([new_location[0], new_location[1]])   # removes urchin from urchin array
+                    CA.nb_urchin-=1
+                else:
+                    CA.empty.remove([new_location[0], new_location[1]])  # delete from empty
+                CA.grid[new_location[0]][new_location[1]] = Otter([new_location[0], new_location[1]])  #spawns new otter
                 CA.nb_otter += 1
         return CA
 
     def death(self, CA):
         if super().death(CA):
             CA.nb_otter -= 1
-        # delete(self)
         return CA
-
-
-# for testing
 
 def main():
     myBoard = CA()
-    i = 1000
+    i =0
     screen = myBoard.create_display()
-    while i > 0:
+    kel=[] # kelp array for plot
+    urch=[] # urchin array for plot
+    ott=[] # otter array for plot
+    x=[]
+    while i < 100: # set number of cycles
         myBoard.updating()
         myBoard.display(screen)
-        time.sleep(0.1)
-        i -= 1
+        kel.append(myBoard.nb_kelp)
+        urch.append(myBoard.nb_urchin)
+        ott.append(myBoard.nb_otter)
+        x.append(i)
 
+        time.sleep(0.01)
+        i += 1
+
+    # plots kelp vs urchin vs otter
+    plt.plot(x,kel,label='Kelp')
+    plt.plot(x,urch,label='Urchin')
+    plt.plot(x,ott,label='Otter')
+    plt.xlabel('time')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
 
 main()
 
